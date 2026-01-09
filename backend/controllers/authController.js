ce36073945821c3e7743b354beb50608df0ede2d
oriviyan-claptales <oriviyan@gmail.com>
2026-01-09 14:04:48 +0530

    Update authController.js
    add cokkey condition

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -173,11 +173,19 @@ export const verifyLoginOTP = async (req, res) => {
     user.failedLoginAttempts = 0;
     await user.save();
 
+    // const token = await genToken(user._id);
+    // res.cookie("token", token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
+
     const token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -353,10 +361,16 @@ export const signOut = async (req, res) => {
     }
 
     // 🛑 STEP 2: Clear Cookie
-    res.clearCookie("token", {
+    // res.clearCookie("token", {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production", // Production me true
+    //   sameSite: "strict",
+    // });
+
+     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Production me true
-      sameSite: "strict",
+      secure: true, // Production me true
+      sameSite: "none",
     });
 
     return res.status(200).json({
