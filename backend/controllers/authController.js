42794f52eeafc0e875f5a59a149e543d9293b64d
oriviyan-claptales <oriviyan@gmail.com>
2026-01-13 11:58:36 +0530

    Update authController.js
    authcontorler change cookis

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -841,7 +841,8 @@ export const verifyLoginOTP = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      // sameSite: "lax",
+      sameSite: "none",
       domain: ".oriviyan.com",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
@@ -976,7 +977,7 @@ export const signOut = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Production me true
-      sameSite: "strict",
+      sameSite: "none",
     });
 
     return res.status(200).json({
