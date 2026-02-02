bf10c71d0f4dbabe0e286d0ef6c0055e6e3e6ece
oriviyan-claptales <oriviyan@gmail.com>
2026-02-02 13:31:13 +0530

    Update Product.js
    add gun cetagory

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,3 +1,52 @@
+// import mongoose from "mongoose";
+
+// const ProductSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   description: { type: String },
+
+//   category: {
+//     type: String,
+//     enum: ["Die-cast", "Remote Control", "Soft Toy", "Board Game", "Scooter"],
+//     required: true
+//   },
+
+//   color: {
+//     type: String,
+//     // enum: ["Black", "White", "Red","Pink", "Blue", "Green", "Yellow", "Orange", "Brown", "Grey"],
+//     required: true
+//   },
+
+//   // 👉 universal size field (text me store hoga: "15 cm", "10 inch", "1:32", "S", "L", etc.)
+//   size: { type: String, required: true },
+
+//   img: { type: String },
+//   price: { type: Number, required: true },
+//   costing_price: {
+//     type: Number,
+//     required: true
+//   },
+//   gst: { type: Number, required: true },
+
+//   sku: { type: String, unique: true, index: true },
+//   barcodeImg: { type: String }, //
+
+//   Supplier_name: { type: String, required: true },
+//   Qty: { type: Number, default: 0 },
+
+// }, { timestamps: true });
+
+// export default mongoose.model("Product", ProductSchema);
+
+
+
+
+
+
+
+
+
+
+
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
@@ -6,7 +55,7 @@ const ProductSchema = new mongoose.Schema({
 
   category: {
     type: String,
-    enum: ["Die-cast", "Remote Control", "Soft Toy", "Board Game", "Scooter"],
+    enum: ["Die-cast", "Remote Control", "Soft Toy", "Board Game", "Scooter","Gun"],
     required: true
   },
 
