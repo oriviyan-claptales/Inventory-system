61d3c329c605a98a3825a1c310d4d63a466594c1
oriviyan-claptales <oriviyan@gmail.com>
2026-02-12 17:35:48 +0530

    Update User.js
    comment username in model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String },
+    // username: { type: String },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
