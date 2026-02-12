7d999a2a224a0b8081bcdeb573cb80b266d9a752
oriviyan-claptales <oriviyan@gmail.com>
2026-02-12 17:06:11 +0530

    Update User.js
    change user model remove user name

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
