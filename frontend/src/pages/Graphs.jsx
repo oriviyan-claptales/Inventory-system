48b5cd7cf0748e7207e892b7de3b522679540421
oriviyan-claptales <oriviyan@gmail.com>
2026-03-25 17:48:31 +0530

    Update Graphs.jsxchange url graph

--- frontend/src/pages/Graphs.jsx
+++ frontend/src/pages/Graphs.jsx
@@ -15,7 +15,7 @@
 //   }, []);
 
 //   const fetchData = async () => {
-//     const res = await axios.get("http://localhost:7000/api/products");
+//     const res = await axios.get("https://inventory-system-uvj3.onrender.com/api/products");
 //     setProducts(res.data);
 //   };
 
@@ -183,7 +183,7 @@ export default function Graphs() {
   const fetchData = async () => {
     try {
       // Agar aapke paas 'api' instance hai to use use karein, warna axios theek hai
-      const res = await axios.get("http://localhost:7000/api/products", {
+      const res = await axios.get("https://inventory-system-uvj3.onrender.com/api/products", {
          withCredentials: true 
       });
       setProducts(res.data);
