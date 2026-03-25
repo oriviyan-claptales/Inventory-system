92feec4eef3308bdf8d3a4934d5eeb210814e944
oriviyan-claptales <oriviyan@gmail.com>
2026-03-25 17:34:22 +0530

    Update axios.js
    change base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api",
+  // baseURL: "http://localhost:7000/api",
+  baseURL: "https://inventory-system-uvj3.onrender.comapi",
   withCredentials: true, // if cookies
 });
 
